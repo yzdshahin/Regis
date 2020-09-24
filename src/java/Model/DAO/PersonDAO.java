cc8e60279991ae81780c80bba2a850ea72070bba
@yzdshahin <shahin234yazdani@gmail.com>
2020-09-24 19:34:37 +0330

    Crypto

--- src/java/Model/DAO/PersonDAO.java
+++ src/java/Model/DAO/PersonDAO.java
@@ -7,27 +7,48 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.security.NoSuchAlgorithmException;
+import javax.websocket.Decoder;
 
 
 public class PersonDAO {
     
-    public static boolean validateUser(String nameUser) throws SQLException {
-        boolean validate= false;
-           try{ ConnectionProvider connectioner2 =new ConnectionProvider();
-            PreparedStatement   preparedStatement = connectioner2.getConnection().prepareStatement("SELECT * from Employee where user=?");
+//    public static boolean validateUser(String nameUser) throws SQLException {
+//        boolean validate= false;
+//           try{ ConnectionProvider connectioner2 =new ConnectionProvider();
+//            PreparedStatement   preparedStatement = connectioner2.getConnection().prepareStatement("SELECT * from Employee where user=?");
+//            preparedStatement.setString(1, "name");
+//            ResultSet resultSet2= preparedStatement.executeQuery();
+//            validate=resultSet2.next();
+//           }catch(SQLException e){
+//               System.out.println("the user you entered is not registered");}
+//           
+//        return validate;
+//    }
+        public static boolean getinfo(String name, String password, String father,String birth, String gender){
+            boolean info=false;
+             try{
+                 ConnectionProvider connectioner1 =new ConnectionProvider();
+            PreparedStatement   preparedStatement = connectioner1.getConnection().prepareStatement("insert  into Employee values (?,?,?,?,?)");
+
             preparedStatement.setString(1, "name");
+            preparedStatement.setString(2, "password");
+            preparedStatement.setString(3, "father");
+            preparedStatement.setString(4, "birth");
+            preparedStatement.setString(5,"gender");
+            
             ResultSet resultSet2= preparedStatement.executeQuery();
-            validate=resultSet2.next();
-           }catch(SQLException e){
-               System.out.println("the user you entered is not registered");}
-           
-        return validate;
-    }
+
+            info=resultSet2.next();
+                 System.out.println("You are successfully registered");
+             }catch(SQLException e){System.out.println("enter all the data");}
+        return info;
+        
+        }
     
         public static boolean validatePass(String PassUser) throws SQLException {
         boolean validation= false;
            try{ ConnectionProvider connectioner2 =new ConnectionProvider();
-            PreparedStatement   preparedStatement = connectioner2.getConnection().prepareStatement("SELECT * from Employee where pass=?");
+            PreparedStatement   preparedStatement = connectioner2.getConnection().prepareStatement("SELECT * from Employee where password=?");
             preparedStatement.setString(1, "password");
             ResultSet resultSet3= preparedStatement.executeQuery();
             validation=resultSet3.next();
@@ -56,7 +77,7 @@ public class PersonDAO {
 //        return vaild;
 //    }
 
-    private static boolean SQLException(SQLException e) {
+    private  boolean SQLException(SQLException e) {
         throw new UnsupportedOperationException("Not supported yet."); 
     }
 
--- src/java/Model/Entity/Person.java
+++ src/java/Model/Entity/Person.java
@@ -6,6 +6,36 @@ public class Person {
     
     private String name;
     private String password;
+    private String father;
+    private String birth;
+    private String gender;
+  
+
+    public String getFather() {
+        return father;
+    }
+
+    public void setFather(String father) {
+        this.father = father;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+      
+        this.birth = birth;
+    }
+
+    public String isGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+      
+        this.gender = gender;
+    }
 
     public String getName() {
         return name;
